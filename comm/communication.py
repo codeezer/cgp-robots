a0a52b777f4d17434364a2d5b1a96022ac2086f6
Anuj Gautam <anuj_gtm1@hotmail.com>
2016-05-12 19:49:02 +0545

    Communication part

---
+++ comm/communication.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import serial
+
+bot1 = serial.Serial('/dev/rfcomm1', 9600)
+bot1.timeout = 1
+# bot2 = serial.Serial('/dev/rfcomm2', 9600)
+
+bot1.write('Something')  # Comm with bot1
+# bot2.write('Something') # Comm with bot2
+
+# bot1.readline()  # Read from bot1
+# bot2.readline() # Read from bot2
